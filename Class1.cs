4d8e80c853366534865168794c97755cb98aae3c
gilljaspreetsingh <50706258+gilljaspreetsingh@users.noreply.github.com>
2019-05-23 10:49:34 -0400

    Add files via upload
    VALUE CHANGED 10+20 =30

--- Class1.cs
+++ Class1.cs
@@ -13,11 +13,11 @@ namespace AwesomeCalculator
         private double number1;
 
         [Test]
-        public void GetAddition_Input2point0and3point0_Returns5point0()
+        public void GetAddition_Input10point0and20point0_Returns30point0()
         {//Arrange
 
-            double number1 = 2;
-            double number2 = 3;
+            double number1 = 10;
+            double number2 = 20;
             double expectedResult = number1 + number2;
             Calc testCalc = new Calc(number1, number2);
             //Act double actualResult = testCalc.GetAddition();
