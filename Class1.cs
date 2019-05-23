105bd11b5761ee55686c045b747fb76a43b0db09
gilljaspreetsingh <50706258+gilljaspreetsingh@users.noreply.github.com>
2019-05-23 10:37:44 -0400

    Add files via upload
    THIS IS A FIRST ADDITION FILE

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AwesomeCalculator;
+using NUnit.Framework;
+
+namespace AwesomeCalculator
+{
+    public class Class1
+    {
+        private double number1;
+
+        [Test]
+        public void GetAddition_Input3point4and5point6_Returns9point0()
+        {//Arrange
+
+            double number1 = 9.4;
+            double number2 = 6.6;
+            double expectedResult = number1 + number2;
+            Calc testCalc = new Calc(number1, number2);
+            //Act double actualResult = testCalc.GetAddition();
+            //Assert Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
+
